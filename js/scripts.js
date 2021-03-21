b98f6dd57ec52146fa3f642ab0986756d4dcfc1d
JasonVolkoff <jasonvolkoff@mail.fresnostate.edu>
2021-03-20 23:33:03 -0700

    Worked on looping through api data.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
+// $(document).ready(function () {
+//     $.get("https://randomuser.me/api/?results=5", function (data) {
+//         $("#name1").text(data.results[0].name.title + " " + data.results[0].name.first + " " + data.results[0].name.last);
+//         $("#age1").text(data.results[0].dob.age);
+//         $("#email1").text(data.results[0].email);
+//         $("#address1").text(`${data.results[0].location.street.number} ${data.results[0].location.street.name}, ${data.results[0].location.city}, ${data.results[0].location.state} ${data.results[0].location.postcode}`);
+//         console.log(data.results[0]);
+//     }, "json");
 $(document).ready(function () {
-    $.get("https://randomuser.me/api/?results=5", function (data) {
-        $("#name1").text(data.results[0].name.title + " " + data.results[0].name.first + " " + data.results[0].name.last);
-        $("#age1").text(data.results[0].dob.age);
-        $("#email1").text(data.results[0].email);
-        $("#address1").text(`${data.results[0].location.street.number} ${data.results[0].location.street.name}, ${data.results[0].location.city}, ${data.results[0].location.state} ${data.results[0].location.postcode}`);
-        console.log(data.results[0]);
-    }, "json");
+    $.getJSON("https://randomuser.me/api/?results=5", function (data) {
+        var randomuser_data = data;
+        $.each(randomuser_data[0].results, function (key, value) {
+            console.log(key);
+            console.log(value);
+
+        });
+    });
 });
