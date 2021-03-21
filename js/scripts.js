73fc3e0c3628b1175567766baee042124e96fb8f
JasonVolkoff <jasonvolkoff@mail.fresnostate.edu>
2021-03-21 01:56:41 -0700

    Completed table portion; will work on bootstrap cards next.

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,29 @@
 //         console.log(data.results[0]);
 //     }, "json");
 $(document).ready(function () {
-    $.getJSON("https://randomuser.me/api/?results=5", function (data) {
-        var randomuser_data = data;
-        $.each(randomuser_data[0].results, function (key, value) {
-            console.log(key);
-            console.log(value);
+    $.ajax({
+        url: 'https://randomuser.me/api/?results=5',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data.results[0]);
+            for (var i = 0; i < 5; i++) {
+                $("#userTables").append("<tr>" +
+                    "<td>" +
+                    data.results[i].name.title + " " + data.results[i].name.first + " " + data.results[i].name.last +
+                    "</td>" +
+                    "<td>" +
+                    data.results[i].dob.age +
+                    "</td>" +
+                    "<td>" +
+                    data.results[i].email +
+                    "</td>" +
+                    "<td>" +
+                    `${data.results[0].location.street.number} ${data.results[0].location.street.name}, ${data.results[0].location.city}, ${data.results[0].location.state} ${data.results[0].location.postcode}` +
+                    "</td>" +
+                    "</tr>"
+                );
 
-        });
+            }
+        }
     });
-});
+})
